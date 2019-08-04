04d34238d77eaef133c712a75c8584d8f580fc57
HP <359458021@qq.com>
2019-08-04 23:05:55 +0800

    模板

---
+++ src/main/java/com/ucarinc/bean/Student.java
@@ -0,0 +1,50 @@
+package com.ucarinc.bean;
+
+public class Student {
+    private String id;
+    private String name;
+    private String sex;
+    private String addr;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", sex='" + sex + '\'' +
+                ", addr='" + addr + '\'' +
+                '}';
+    }
+}
---
+++ src/test/java/test.java
@@ -0,0 +1,16 @@
+import com.ucarinc.bean.Student;
+import javafx.application.Application;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
+
+public class test {
+    public static void main(String[] args) {
+        ApplicationContext ac =new ClassPathXmlApplicationContext("applicationContext.xml");
+        SqlSessionTemplate sqlSession = ac.getBean("sqlSession", SqlSessionTemplate.class);
+        List<Student> list = sqlSession.selectList("getStudentList");
+        System.out.println(list);
+    }
+}
